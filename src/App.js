04440cf725369fd617b076b6b82627bbe8226464
CRHarding <34920615+CRHarding@users.noreply.github.com>
2022-07-01 13:05:48 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import WeatherDisplay from "./WeatherDisplay";
 
 function App() {
-  const [openWeather, setOpenWeather] = useState(null);
+  const [openWeather, setOpenWeather] = useState("Fake data for now!");
   const [userData, setUserData] = useState({ name: "Casey Harding", age: 74, bestInstructor: true });
 
   return (
